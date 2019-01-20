34d50007eb72ad78d6a0ff61ffda94fe05a4c259
Oleg <Martynov>
2019-01-20 18:01:06 +0300

    first commit

---
+++ goodfonDownloader/Controllers/HomeController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using HtmlAgilityPack;
+using Newtonsoft.Json;
+
+namespace goodfonDownloader.Controllers
+{
+
+
+    public class HomeController : Controller
+    {
+        private string downloadImage(string urlAdress, string category, string index = null)
+        {
+            try
+            {
+
+
+                HtmlDocument docWithImage = new HtmlDocument();
+
+                using (var wb = new WebClient())
+                {
+                    var response = wb.DownloadString(urlAdress);
+                    docWithImage = new HtmlDocument();
+                    docWithImage.LoadHtml(response);
+                    wb.Dispose();
+                }
+
+                string url = docWithImage.GetElementbyId("im").Attributes["href"].Value;
+
+                using (var webClient = new WebClient())
+                {
+                    string path_to_image = Server.MapPath("~/App_Data/" + category + "/" + url.Split('/').Last());
+                    webClient.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
+                    webClient.DownloadFile(url, path_to_image);
+                    webClient.Dispose();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+
+            return "ok";
+        }
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string downloadCategoryImagesFromPage(string imageLinks, string category)
+        {
+            try
+            {
+                var imageLinksList = JsonConvert.DeserializeObject<List<string>>(imageLinks);
+
+                if (!Directory.Exists(Server.MapPath("~/App_Data/" + category + "/")))
+                {
+                    Directory.CreateDirectory(Server.MapPath("~/App_Data/" + category + "/"));
+                }
+
+                downloadImage(imageLinksList.First(), category);
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+            return "ok";
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ goodfonDownloader/Scripts/main.js
@@ -0,0 +1,141 @@
+﻿
+let doc = document;
+
+var doctype = document.implementation.createDocumentType('html', '', '');
+var dom = document.implementation.createDocument('', 'html', doctype);
+
+
+$.ajaxPrefilter(function (options) {
+    if (options.crossDomain && jQuery.support.cors) {
+        var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+        options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+        //options.url = "http://cors.corsproxy.io/url=" + options.url;
+    }
+});
+
+let url_site = "https://www.goodfon.ru/";
+let categoriesImages = [];
+
+
+const get_categories_images = function (html) {
+
+    doc.getElementById('bodyHtml').innerHTML = html;
+    
+    let div_head_menu_elements = doc.querySelector('div.head_menu').children;
+    Array.prototype.forEach.call(div_head_menu_elements, (element) => {
+        $('#categoriesImages')
+            .append($("<option></option>")
+                .attr("value", element.href)
+                .text(element.innerHTML)); 
+    });
+};
+
+
+const form_link_images_category_page = function (image_names_from_category_page) {
+
+    let links_download_images_category_page = [];
+
+    image_names_from_category_page.forEach((image) => {
+        let link_for_image = `${url_site}download/${image}/1366x768/`;
+        links_download_images_category_page.push(link_for_image);
+    });
+    return links_download_images_category_page;
+};
+
+const get_image_names_from_category_page = function (html) {
+
+    let image_names_from_category_page = [];
+    doc.getElementById('bodyHtml').innerHTML = html;
+
+    let elements = doc.querySelectorAll('div.wallpapers__item__wall');
+    Array.prototype.forEach.call(elements, (element) => {
+        let name_image = element.children[0].href.split('/').pop().split('.')[0];
+        image_names_from_category_page.push(name_image);
+    });
+    return image_names_from_category_page;
+};
+
+
+$("#download_section").click(function () {
+    
+    $.get($("#categoriesImages option:selected").val(), function (response) {
+        let image_names_from_category_page = get_image_names_from_category_page(response);
+        let link_images_category_page = form_link_images_category_page(image_names_from_category_page);
+
+        $('#processingImagesDownload').css('display', 'block');
+
+        //берем файл из input
+        var formData = new FormData();
+        formData.append('imageLinks', JSON.stringify(link_images_category_page));
+        formData.append('category', $("#categoriesImages option:selected").text());
+
+        $.ajax({
+            type: "POST",
+            url: "/Home" + "/downloadCategoryImagesFromPage",
+            data: formData,
+            contentType: false,
+            processData: false,
+            async: true,
+            success: function (response, textStatus) {
+                if (response === "ok") {
+
+                }
+            },
+            error: function (request, status, error) {
+                alert('error');
+            }
+        });
+
+    });
+
+});
+
+
+$.get(url_site, function (response) {
+    get_categories_images(response);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//$.ajax({
+//        type: 'GET',
+//        url: url_site,
+//        //proxy: 'http://evil.com/', 
+//        headers: { 'Access-Control-Allow-Origin': '*' },
+//        //beforeSend: function (xhr) {
+//        //    xhr.setRequestHeader('cache-control','max-age=0');
+//        //    xhr.setRequestHeader('upgrade-insecure-requests','1');
+//        //    xhr.setRequestHeader('method','GET');
+//        //},
+//        accepts: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+//        //contentType: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+//        //crossDomain: true,
+//        //cache: true,
+//        //processData: false,
+//        crossOrigin: true,
+//        async: true,
+//        success: function (response, textStatus) {
+//        console.log(response);
+//        //get_categories_images(response);
+//        },
+//    error: function (request, status, error) {
+//        alert(error);
+//        }
+//    });
